171476d8a9b12c4c2977e91b8cb3c85d5e2fb268
user <user@CVTE-2.local>
2019-12-03 10:06:49 +0800

    egg基本搭建

---
+++ config/config.default.js
@@ -0,0 +1,54 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1575269908956_2905';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+//配置mysql
+config.mysql = {
+  client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '128038zy',
+      // 数据库名
+      database: 'Test',
+  },
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+}
+config.security = {
+  csrf: {
+    enable: false,
+  },
+};
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
---
+++ config/plugin.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+// module.exports = {
+  // enable: true,
+  // package: 'egg-mysql',
+// };
+// config/plugin.js
+exports.mysql = {
+  enable: true,// 开启
+  package: 'egg-mysql', // 对应哪个包
+};
+
